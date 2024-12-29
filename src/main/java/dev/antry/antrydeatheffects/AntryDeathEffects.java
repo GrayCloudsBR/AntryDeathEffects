64f107796d9b00afa3b973e704a0417510173e8f
GrayClouds <graycloudsbr@gmail.com>
2024-12-29 14:19:05 +0000

    Initial commit

---
+++ src/main/java/dev/antry/antrydeatheffects/AntryDeathEffects.java
@@ -0,0 +1,17 @@
+package dev.antry.antrydeatheffects;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntryDeathEffects extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
